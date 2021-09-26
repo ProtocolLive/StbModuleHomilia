a271cd7efff84f653da1c1173f87ebf218add9c0
Protocol Corporation <protocollive@outlook.com>
2021-09-26 07:28:33 -0300

    Webhook movido para os globais

--- src/homilia/index.php
+++ src/homilia/index.php
@@ -1,10 +1,11 @@
 <?php
-//2021.04.25.01
+//2021.09.26.00
 //Protocol Corporation Ltda.
 //https://github.com/ProtocolLive/SimpleTelegramBot
 
-function Command_homilia(TblCmd $Webhook):void{
+function Command_homilia():void{
   DebugTrace();
+  global $Webhook;
   $msg = file_get_contents(__DIR__ . '/homilia.txt');
   $msg = str_replace('##NAME##', $Webhook->User->Name, $msg);
   $msg = explode('##BREAK##', $msg);
@@ -14,9 +15,9 @@ function Command_homilia(TblCmd $Webhook):void{
   LogEvent('homilia');
 }
 
-function Command_homiliaset(TblCmd $Webhook):void{
+function Command_homiliaset():void{
   DebugTrace();
-  global $DB;
+  global $DB, $Webhook;
   if($DB->Admin($Webhook->User->Id)):
     file_put_contents(__DIR__ . '/homilia.txt', $Webhook->Parameters);
     $Webhook->ReplyMsg('👍');
